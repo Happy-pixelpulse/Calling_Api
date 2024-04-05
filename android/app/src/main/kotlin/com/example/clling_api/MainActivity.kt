adeb462e47af34c59058571c1bef8bc2164346cc
happy_shah <happynshah@gmail.com>
2024-04-05 15:49:06 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/clling_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clling_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login.dart
@@ -0,0 +1,149 @@
+// import 'package:flutter/material.dart';
+//
+// class Login extends StatefulWidget {
+//   const Login({super.key});
+//
+//   @override
+//   State<Login> createState() => _LoginState();
+// }
+//
+// class _LoginState extends State<Login> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       bottomNavigationBar: Padding(
+//         padding: const EdgeInsets.only(left: 20, right: 20),
+//         child: ElevatedButton(
+//           style: ElevatedButton.styleFrom(
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(15),
+//             ),
+//             padding: const EdgeInsets.symmetric(
+//               horizontal: 100,
+//             ),
+//             backgroundColor: Color.fromRGBO(64, 82, 224, 1),
+//           ),
+//           onPressed: () {
+//
+//           },
+//           child: Text("Submit"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+
+import 'HomePage.dart';
+//import 'HomePage.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  List<dynamic> Users = [];
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leadingWidth: 100,
+        leading: GestureDetector(
+          child: const Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Row(
+              children: [
+                Icon(Icons.arrow_back_ios, color: Colors.black87),
+                Text('Back', style: TextStyle(color: Colors.black)),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20,top: 80),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+           mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                suffixIcon: Icon(Icons.email_rounded),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                } else if (!RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$')
+                    .hasMatch(value)) {
+                  return 'Please enter a valid email address';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              width: 12,
+            ),
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                suffixIcon: Icon(
+                  Icons.key,
+                  color: Colors.grey,
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                } else if (!RegExp(r'^(?=.*[@])(?=.*[a-zA-Z0-9]).{8,}$')
+                    .hasMatch(value)) {
+                  return 'Password must be at least 8 characters long and contain @ symbol';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 155,
+                ),
+                backgroundColor: Color.alphaBlend(Colors.red, Colors.redAccent),
+              ),
+              onPressed: (){
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const HomePage()),
+                );
+              },
+              child: const Text('login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
